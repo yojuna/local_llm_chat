c2e231d96b50c956dd5ec4f8001dc224888a10cd
yojuna <datamongeraami@gmail.com>
2024-05-26 01:27:21 +0200

    cleanup

--- pages/1_web_scraper.py
+++ pages/1_web_scraper.py
@@ -116,7 +116,7 @@ def save_email(email):
         file.write(email + "\n")
 
 with st.sidebar:
-    st.write("Official demo for [Scrapegraph-ai](https://github.com/VinciGit00/Scrapegraph-ai) library")
+    # st.write("Official demo for [Scrapegraph-ai](https://github.com/VinciGit00/Scrapegraph-ai) library")
     st.markdown("""---""")
     st.write("# Usage Examples")
     st.write("## Prompt 1")
@@ -130,10 +130,10 @@ with st.sidebar:
     # st.markdown("""---""")
     # st.write("You want to suggest tips or improvements? Contact me through email to mvincig11@gmail.com")
 
-st.title("Scrapegraph-ai")
-left_co, cent_co, last_co = st.columns(3)
-with cent_co:
-    st.image("assets/scrapegraphai_logo.png")
+st.title("SMART WEB CRAWLER ")
+# left_co, cent_co, last_co = st.columns(3)
+# with cent_co:
+#     st.image("assets/scrapegraphai_logo.png")
 
 # key = st.text_input("Openai API key", type="password")
 key = st.secrets.openai_key
@@ -171,34 +171,34 @@ if st.button("Run the program", type="primary"):
             if graph_result:
                 add_download_options(graph_result)
 
-left_co2, *_, cent_co2, last_co2, last_c3 = st.columns([1] * 18)
+# left_co2, *_, cent_co2, last_co2, last_c3 = st.columns([1] * 18)
 
-with cent_co2:
-    discord_link = "https://discord.com/invite/gkxQDAjfeX"
-    discord_logo = base64.b64encode(open("assets/discord.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{discord_link}" target="_blank">
-        <img src="data:image/png;base64,{discord_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
+# with cent_co2:
+#     discord_link = "https://discord.com/invite/gkxQDAjfeX"
+#     discord_logo = base64.b64encode(open("assets/discord.png", "rb").read()).decode()
+#     st.markdown(
+#         f"""<a href="{discord_link}" target="_blank">
+#         <img src="data:image/png;base64,{discord_logo}" width="25">
+#         </a>""",
+#         unsafe_allow_html=True,
+#     )
 
-with last_co2:
-    github_link = "https://github.com/VinciGit00/Scrapegraph-ai"
-    github_logo = base64.b64encode(open("assets/github.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{github_link}" target="_blank">
-        <img src="data:image/png;base64,{github_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
+# with last_co2:
+#     github_link = "https://github.com/VinciGit00/Scrapegraph-ai"
+#     github_logo = base64.b64encode(open("assets/github.png", "rb").read()).decode()
+#     st.markdown(
+#         f"""<a href="{github_link}" target="_blank">
+#         <img src="data:image/png;base64,{github_logo}" width="25">
+#         </a>""",
+#         unsafe_allow_html=True,
+#     )
 
-with last_c3:
-    twitter_link = "https://twitter.com/scrapegraphai"
-    twitter_logo = base64.b64encode(open("assets/twitter.png", "rb").read()).decode()
-    st.markdown(
-        f"""<a href="{twitter_link}" target="_blank">
-        <img src="data:image/png;base64,{twitter_logo}" width="25">
-        </a>""",
-        unsafe_allow_html=True,
-    )
+# with last_c3:
+#     twitter_link = "https://twitter.com/scrapegraphai"
+#     twitter_logo = base64.b64encode(open("assets/twitter.png", "rb").read()).decode()
+#     st.markdown(
+#         f"""<a href="{twitter_link}" target="_blank">
+#         <img src="data:image/png;base64,{twitter_logo}" width="25">
+#         </a>""",
+#         unsafe_allow_html=True,
+#     )
